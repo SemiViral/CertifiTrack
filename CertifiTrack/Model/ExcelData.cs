00c2548b9d2de73f897a199a45cdb9b6ce2eaf4a
Alejandro Virule <semiviral@gmail.com>
2016-03-26 04:11:56 -0500

    Fix releasing file after update

--- CertifiTrack/Model/ExcelData.cs
+++ CertifiTrack/Model/ExcelData.cs
@@ -10,43 +10,29 @@ using OfficeOpenXml;
 namespace CertifiTrack.Model {
     internal class ExcelData {
         public static IEnumerable<DeathCertificate> GetCertificates(string filePath) {
-            var certList = new List<DeathCertificate>();
+            using (ExcelPackage package = new ExcelPackage())
+            using (FileStream file = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)) {
+                package.Load(file);
 
-            ExcelPackage package = new ExcelPackage();
+                ExcelWorksheet worksheet = package.Workbook.Worksheets["2016 DC Info"];
 
-            FileStream file = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                for (int row = 3; row <= worksheet.Dimension.End.Row; row++) {
+                    if (worksheet.Cells[row,
+                        1].Value == null) continue;
 
-            package.Load(file);
+                    DeathCertificate cert = new DeathCertificate(Convert.ToDouble(worksheet.Cells[row, 1].Value),
+                        worksheet.Cells[row, 2].Value, worksheet.Cells[row, 3].Value, worksheet.Cells[row, 4].Value,
+                        worksheet.Cells[row, 5].Value, worksheet.Cells[row, 6].Value, worksheet.Cells[row, 7].Value,
+                        worksheet.Cells[row, 8].Value, worksheet.Cells[row, 9].Value, worksheet.Cells[row, 10].Value,
+                        worksheet.Cells[row, 11].Value, worksheet.Cells[row, 12].Value);
 
-            ExcelWorksheet worksheet = package.Workbook.Worksheets["2016 DC Info"];
-
-            certList.Clear();
-
-            for (int row = 3; row <= worksheet.Dimension.End.Row; row++) {
-                if (worksheet.Cells[row, 1].Value == null) continue;
-
-                DeathCertificate cert = new DeathCertificate(
-                    Convert.ToDouble(worksheet.Cells[row, 1].Value),
-                    worksheet.Cells[row, 2].Value,
-                    worksheet.Cells[row, 3].Value,
-                    worksheet.Cells[row, 4].Value,
-                    worksheet.Cells[row, 5].Value,
-                    worksheet.Cells[row, 6].Value,
-                    worksheet.Cells[row, 7].Value,
-                    worksheet.Cells[row, 8].Value,
-                    worksheet.Cells[row, 9].Value,
-                    worksheet.Cells[row, 10].Value,
-                    worksheet.Cells[row, 11].Value,
-                    worksheet.Cells[row, 12].Value
-                    );
-
-                if ((cert.Notes == null) ||
-                    (!cert.Notes.Contains("Done") && !cert.Notes.Contains("Out of State") && !cert.Notes.Contains("Rental") &&
-                     !cert.Notes.Contains("No DC")))
-                    yield return cert;
+                    if (cert.Notes == null ||
+                        (!cert.Notes.Contains("Done") && !cert.Notes.Contains("Out of State") &&
+                         !cert.Notes.Contains("Rental") &&
+                         !cert.Notes.Contains("No DC")))
+                        yield return cert;
+                }
             }
-
-            package.Dispose();
         }
 
         //public static string FormatDisplayString(string date, string name, string status, bool isApproved, bool isElectronic,
